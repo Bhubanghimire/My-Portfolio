527d6b77633b5fe6129d2306c56f636551c48bda
bhuban <bhubanghimire3130@gmail.com>
2022-10-01 19:16:45 +0545

    cover update

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,21 +1,23 @@
 from django.contrib import admin
-from .models import User, Contact, Address, About, Skills, Testimonials, Services, Education, Experience, Portfolio, Gallery
+from .models import User, Contact, Address, About, Skills, Testimonials, Services, Education, Experience, Portfolio, \
+    Gallery
 
 
 # Register your models here.
 @admin.register(User)
 class MyUserAdmin(admin.ModelAdmin):
-    list_display = ["id",'email', 'first_name', 'last_name']
+    list_display = ["id", 'email', 'first_name', 'last_name']
 
 
 @admin.register(Contact)
 class MyUserAdmin(admin.ModelAdmin):
-    list_display = ["id",'first_name', 'subject', 'message']
+    list_display = ["id", 'first_name', 'subject', 'message']
 
 
 @admin.register(Address)
 class MyUserAdmin(admin.ModelAdmin):
-    list_display = ["id","country","district","city"]
+    list_display = ["id", "country", "district", "city"]
+
 
 @admin.register(About)
 class AboutAdmin(admin.ModelAdmin):
@@ -25,6 +27,7 @@ class AboutAdmin(admin.ModelAdmin):
 @admin.register(Skills)
 class AboutAdmin(admin.ModelAdmin):
     list_display = ["id"]
+
 
 @admin.register(Testimonials)
 class AboutAdmin(admin.ModelAdmin):
@@ -40,9 +43,11 @@ class AboutAdmin(admin.ModelAdmin):
 class AboutAdmin(admin.ModelAdmin):
     list_display = ["id"]
 
+
 @admin.register(Experience)
 class AboutAdmin(admin.ModelAdmin):
     list_display = ["id"]
+
 
 @admin.register(Portfolio)
 class AboutAdmin(admin.ModelAdmin):
