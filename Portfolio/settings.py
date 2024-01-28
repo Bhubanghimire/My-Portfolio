d7cd4bf07416a11a860882e7553008d2805e8bfd
bhuban <bhuban@nsdai.me>
2024-01-28 20:15:07 +0545

    push

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -131,6 +131,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 
 
