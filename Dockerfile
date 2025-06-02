a65c58ac22f8047294b83b5a1650443fdca28b21
bhuban <bhuban@nsdai.me>
2025-06-02 11:06:25 +0545

    merge complete

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# Dockerfile
+FROM python:3.10-slim
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR /app
+
+COPY requirements.txt /app/
+RUN pip install --upgrade pip && pip install -r requirements.txt
+
+COPY . /app/
+
+EXPOSE 8005
+
+CMD gunicorn Portfolio.wsgi:application --bind 0.0.0.0:8005
--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -29,6 +29,9 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://*.bhubanghimire.com.np','https://*.127.0.0.1']
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+CSRF_TRUSTED_ORIGINS = ['https://www.bhubanghimire.com.np']
+
 
 # Application definition
 
@@ -126,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
@@ -137,8 +140,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_SHOW_URL = 'media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+<<<<<<< Updated upstream
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"#add this line
+=======
+>>>>>>> Stashed changes
 
 
 
